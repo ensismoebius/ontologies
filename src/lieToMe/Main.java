9b2b59e90a56a6d6154950ae2fceb06369793f45
Andre Furlan <ensismoebius@gmail.com>
2019-10-24 12:48:40 -0300

    Loading ontologies successfully

---
+++ src/lieToMe/Main.java
@@ -0,0 +1,27 @@
+package lieToMe;
+
+import java.io.File;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+public class Main {
+
+	public static void main(String[] args) throws OWLOntologyCreationException {
+		File file = new File("ontologies/tellingLies.owl");
+
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
+
+		System.out.println("Module loaded: " + ontology);
+
+		java.util.Set<OWLEntity> entOnt = ontology.getSignature();
+
+		for (OWLEntity a : entOnt) {
+			System.out.println("Entity " + a);
+		}
+	}
+}
