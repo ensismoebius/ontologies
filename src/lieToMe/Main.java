d24e1d69b5b232e12771c407aff89b49170be67b
Andre Furlan <ensismoebius@gmail.com>
2019-10-27 00:31:20 -0300

    Just a touch

--- src/lieToMe/Main.java
+++ src/lieToMe/Main.java
@@ -1,11 +1,19 @@
 package lieToMe;
 
-import java.util.ArrayList;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 public class Main {
 	public static void main(String[] args) {
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
 
 		try {
 			new Window();
