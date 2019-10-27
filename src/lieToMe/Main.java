f2f6de048ccc4f413e8043f9f86164e1e5c233e3
Andre Furlan <ensismoebius@gmail.com>
2019-10-27 00:41:46 -0300

    Erase an unusable file

--- src/lieToMe/Main.java
+++ src/lieToMe/Main.java
@@ -1,13 +1,6 @@
 package lieToMe;
 
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.plaf.metal.DefaultMetalTheme;
-import javax.swing.plaf.metal.MetalLookAndFeel;
-
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
 
 public class Main {
 	public static void main(String[] args) {
